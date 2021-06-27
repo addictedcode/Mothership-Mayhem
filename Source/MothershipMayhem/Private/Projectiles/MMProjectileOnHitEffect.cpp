e82c1c28edf552d732e3ff60414011bd0171bfb9
Addictedcode <richmond_w_ngo@dlsu.edu.ph>
2021-06-28 00:00:27 +0800

    Add Projectile On Hit Effect Mod. Add Projectile On Hit Status Effect.

--- Source/MothershipMayhem/Private/Projectiles/MMProjectileOnHitEffect.cpp
+++ Source/MothershipMayhem/Private/Projectiles/MMProjectileOnHitEffect.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Projectiles/MMProjectileEffectBase.h"
+#include "Projectiles/MMProjectileOnHitEffect.h"
 
